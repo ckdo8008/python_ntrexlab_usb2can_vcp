51013ab0f76cdadc10943c84edc6222126c9f850
ckdo8008 <68054358+ckdo8008@users.noreply.github.com>
2024-07-29 13:13:39 +0900

    소스 저장소 url 추가

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="이 모듈은 ntrexlab의 usb2can VCP제품을 파이썬으로 can모듈을 사용하기 위해서 작성된 인터페이스 모듈입니다.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/ckdo8008/python_ntrexlab_usb2can_vcp",
     py_modules = ["python_ntrexlab_usb2can_vcp"],
     python_requires=">=3.10",
     install_requires=[
